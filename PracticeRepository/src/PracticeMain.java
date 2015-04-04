2378bc524b13b126650a48f594f883f46f4e824e
rkjc <richardkjcross@gmail.com>
2015-04-04 05:33:39 -0700

    added my name and github username

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -2,7 +2,11 @@
 public class PracticeMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// every one needs to add a line of code to this program to 
+		//print out your name and github username
+		
+		System.out.println("Richard Cross = rkjc");
+		
 
 	}
 
