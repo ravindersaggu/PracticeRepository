73d8787453cf74fd414b0a47f9b1a321106a6603
SuperMaddie <mahdiye.jamali@gmail.com>
2015-04-10 22:59:43 -0700

    Added my username

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -5,8 +5,9 @@ public class PracticeMain {
 		// every one needs to add a line of code to this program to 
 		//print out your name and github username
 		
-		System.out.println("Richard Cross = rkjc");
 		
+		System.out.println("Richard Cross = rkjc");
+		System.out.println("Mahdiye Jamali = SuperMaddie");
 
 	}
 
