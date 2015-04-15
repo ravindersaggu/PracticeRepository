69c0c465dfe98f0d850b8d7ac1ceb7acb1d7342e
c1rabbit <mail@calvinthanh.com>
2015-04-15 00:41:37 -0700

    First Commit

---
+++ src/practice/HelloWorld.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		// every one needs to add a line of code to this program to
+		// print out your name and github username
+
+		System.out.println("Richard Cross = rkjc");
+
+		System.out.println("Calvin Thanh = c1rabbit");
+	}
+
+}
