d8388a23ac5c338c6dd06f4a9bc3ccd507b84d20
niky204 <nikitapatel2305@gmail.com>
2015-04-15 18:09:20 -0700

    second commit

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -7,7 +7,7 @@ public class PracticeMain {
 		
 		System.out.println("Richard Cross = rkjc");
 		// added this additional comment
-		System.out.println("Nikita Patel = niky204");
+		System.out.println("Nikita Patel = niky204"); 
 	
 	}
 
