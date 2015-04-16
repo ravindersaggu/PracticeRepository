88a882a25e670090126243625b8e24358bb0b08f
nirajpatel2402 <nirajpatel2402@gmail.com>
2015-04-15 17:24:48 -0700

    First commit

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -7,7 +7,7 @@ public class PracticeMain {
 		
 		System.out.println("Richard Cross = rkjc");
 		
-  		System.out.println("William Salinas = willy1087");
+  		System.out.println("Niraj Patel = nirajpatel2402");
 	
 	}
 
