1b08fdced83fe08b5899eda1a0a14760e01a6fbc
pchavda <chavda.parth@yahoo.com>
2015-04-15 21:26:36 -0700

    Update PracticeMain.java

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -12,8 +12,7 @@ public class PracticeMain {
 		//this is the original master branch with the rkjc-b1 branch merged in
 
 		System.out.println("Mahdiye Jamali = SuperMaddie");
-		System.out.println("Parth Chavda =pchavda");
-
+	
 	
 	}
 
