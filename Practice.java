804aa50f65b84a59d6331ac5b01a99806a31e7b2
ravindersaggu <sagguravindersingh@gmail.com>
2015-04-16 16:13:32 -0700

    Practice.java File

---
+++ Practice.java
@@ -0,0 +1,8 @@
+public class Practice {
+
+	public static void main(String[] args) {
+	        System.out.println("Richard Cross = rkjc");
+
+		System.out.println("Ravinder Singh Saggu = ravindersaggu");
+	}
+}
