95580cea4c44679292276c477ee47cac23e82c99
dhrumil44 <ddd_4488@yahoo.com>
2015-04-15 21:44:22 -0700

    committed to dhrumil44

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -1,20 +1,17 @@
-
 public class PracticeMain {
 
 	public static void main(String[] args) {
-		// every one needs to add a line of code to this program to 
-		//print out your name and github username
-		
+		// every one needs to add a line of code to this program to
+		// print out your name and github username
 
+		// askdjflkasdjflkasdjflk
 		System.out.println("Richard Cross = rkjc");
 		// added this additional comment
-		//added this on branch b1
-		//this is the original master branch with the rkjc-b1 branch merged in
+		// added this on branch b1
+		// this is the original master branch with the rkjc-b1 branch merged in
 
 		System.out.println("Dhrumil Desai = dhrumil44");
-	
 
-	
 	}
 
 }
