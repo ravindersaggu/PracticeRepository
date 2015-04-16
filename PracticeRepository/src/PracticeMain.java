e33af8ca0426edd5a454659feb9e7bc961a66cc8
nirajpatel2402 <nirajpatel2402@gmail.com>
2015-04-15 17:23:43 -0700

    First commit

---
+++ PracticeRepository/src/PracticeMain.java
@@ -0,0 +1,14 @@
+
+public class PracticeMain {
+
+	public static void main(String[] args) {
+		// every one needs to add a line of code to this program to 
+		//print out your name and github username
+		
+		System.out.println("Richard Cross = rkjc");
+		
+  		System.out.println("William Salinas = willy1087");
+	
+	}
+
+}
