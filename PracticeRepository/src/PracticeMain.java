c7471ee259b1ea208eb775f55abfbea1cd897bce
shashank7991 <shashank.r.rathod@gmail.com>
2015-04-15 21:33:15 -0700

    edit1
    edit1

--- PracticeRepository/src/PracticeMain.java
+++ PracticeRepository/src/PracticeMain.java
@@ -2,9 +2,6 @@
 public class PracticeMain {
 
 	public static void main(String[] args) {
-		// every one needs to add a line of code to this program to 
-		//print out your name and github username
-		
 
 		System.out.println("Richard Cross = rkjc");
 		// added this additional comment
